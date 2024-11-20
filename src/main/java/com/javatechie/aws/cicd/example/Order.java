5c2bdc8495383c97e760cfc925872d1e63effae8
Manoj Kumar <manojkumar@CPM-MANOJ.local>
2024-11-20 09:48:50 +0530

    removed lombok

--- src/main/java/com/javatechie/aws/cicd/example/Order.java
+++ src/main/java/com/javatechie/aws/cicd/example/Order.java
@@ -5,14 +5,52 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@ToString
+
 public class Order {
+
+    public Order(int id, String name, int quantity, long price) {
+        this.id = id;
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public void setPrice(long price) {
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public long getPrice() {
+        return price;
+    }
 
     private int id;
     private String name;
     private int quantity;
     private long price;
+
+
 }
